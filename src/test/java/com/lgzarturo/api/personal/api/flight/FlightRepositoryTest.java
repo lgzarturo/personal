f3ec697ed4e362305812ae9fd331d1a3c2c1f9ff
Arturo López <lgzarturo@gmail.com>
2023-05-03 11:40:29 -0500

    test: agregar tickets a flight
    En las pruebas se agregan varios tickets a cada vuelo que se agrega, el objetivo es probar la relación.

--- src/test/java/com/lgzarturo/api/personal/api/flight/FlightRepositoryTest.java
+++ src/test/java/com/lgzarturo/api/personal/api/flight/FlightRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.lgzarturo.api.personal.api.flight;
 
 import com.github.javafaker.Faker;
+import com.lgzarturo.api.personal.api.ticket.Ticket;
+import com.lgzarturo.api.personal.api.ticket.TicketRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,6 +24,8 @@ class FlightRepositoryTest {
 
     @Autowired
     private FlightRepository flightRepository;
+    @Autowired
+    private TicketRepository ticketRepository;
     List<String> flightNumbers = new ArrayList<>();
     List<BigDecimal> prices = List.of(
         BigDecimal.valueOf(1000.0), BigDecimal.valueOf(2000.0), BigDecimal.valueOf(3000.0), BigDecimal.valueOf(4000.0),
@@ -35,17 +40,27 @@ class FlightRepositoryTest {
     @BeforeEach
     void setUp() {
         flightRepository.deleteAll();
+        ticketRepository.deleteAll();
         for (int i=0; i<10; i++) {
             Random random = new Random();
             Faker faker = new Faker();
-            Flight flight = Flight.builder()
-                .flightNumber(faker.code().gtin13())
-                .airline(Airline.values()[i % Airline.values().length])
-                .destinationName(destinations.get(random.nextInt(destinations.size())))
-                .originName(destinations.get(random.nextInt(destinations.size())))
-                .price(prices.get(random.nextInt(prices.size())))
-                .build();
+            Flight flight = new Flight();
+            flight.setFlightNumber(faker.code().gtin13());
+            flight.setAirline(Airline.values()[i % Airline.values().length]);
+            flight.setOriginName(destinations.get(random.nextInt(destinations.size())));
+            flight.setDestinationName(destinations.get(random.nextInt(destinations.size())));
+            flight.setPrice(prices.get(random.nextInt(prices.size())));
             flightRepository.save(flight);
+            int numberOfTickets = random.nextInt(4) + 1;
+            for (int k=0; k<numberOfTickets; k++) {
+                Ticket ticket = new Ticket();
+                ticket.setId(UUID.randomUUID());
+                ticket.setPurchaseDate(LocalDate.now().plusDays(k));
+                ticket.setDepartureDate(LocalDate.now().plusDays(k+1));
+                ticket.setPrice(prices.get(random.nextInt(prices.size())));
+                ticket.setFlight(flight);
+                ticketRepository.save(ticket);
+            }
             flightNumbers.add(flight.getFlightNumber());
             if (priceMap.containsKey(flight.getPrice())) {
                 priceMap.put(flight.getPrice(), priceMap.get(flight.getPrice()) + 1);
@@ -73,6 +88,7 @@ class FlightRepositoryTest {
         String flightNumber = flightNumbers.get(random.nextInt(flightNumbers.size()));
         // When
         Flight flight = flightRepository.selectByFlightNumber(flightNumber).orElse(null);
+        System.out.println(flight);
         // Then
         assertNotNull(flight);
     }
@@ -90,6 +106,7 @@ class FlightRepositoryTest {
         }
         // When
         Set<Flight> flights = flightRepository.selectByPriceGreaterThan(price);
+        flights.forEach(System.out::println);
         // Then
         assertEquals(priceCount, flights.size());
     }
@@ -109,6 +126,7 @@ class FlightRepositoryTest {
         }
         // When
         Set<Flight> flights = flightRepository.selectByPriceLessThan(price);
+        flights.forEach(System.out::println);
         // Then
         assertEquals(priceCount, flights.size());
     }
@@ -138,6 +156,7 @@ class FlightRepositoryTest {
         Set<Flight> flights = flightRepository.selectByPriceBetween(
             minPrice.subtract(BigDecimal.valueOf(0.1)), maxPrice.add(BigDecimal.valueOf(0.1))
         );
+        flights.forEach(System.out::println);
         // Then
         assertEquals(priceCount, flights.size());
     }
@@ -151,6 +170,7 @@ class FlightRepositoryTest {
         assert airline != null;
         // When
         Set<Flight> flights = flightRepository.selectByAirline(airline);
+        flights.forEach(System.out::println);
         // Then
         assertEquals(airlineMap.get(airline), flights.size());
     }
@@ -166,6 +186,7 @@ class FlightRepositoryTest {
         String destinationName = key.split("-")[1];
         // When
         Set<Flight> flights = flightRepository.selectByOriginNameAndDestinationName(originName, destinationName);
+        flights.forEach(System.out::println);
         // Then
         assertEquals(destinationMap.get(key), flights.size());
     }
